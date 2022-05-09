c54256e411705e28cc5519c0cd79a37702b09c93
sarahcasauria <75954115+sarahcasauria@users.noreply.github.com>
2022-05-09 21:12:50 +1000

    Update joins.sql

--- joins.sql
+++ joins.sql
@@ -2,12 +2,16 @@ select * from hf_table;
 
 select * from out_of_school;
 
-select out_of_school.country_code, out_of_school.country_name, out_of_school.out_of_school, hf_table.hf_score
+-- Joining the two tables, ordering by descending Human Freedom Score
+SELECT out_of_school.country_code, out_of_school.country_name, out_of_school.out_of_school, hf_table.hf_score
 FROM out_of_school
 JOIN hf_table
-ON hf_table.country_code=out_of_school.country_code;
+ON hf_table.country_code=out_of_school.country_code
+ORDER BY hf_table.hf_score DESC;
 
-select hf_table.country_code, hf_table.country_name, hf_table.hf_score, out_of_school.out_of_school
+-- Joining the two tables, ordering by descending Out Of School percentage
+SELECT hf_table.country_code, hf_table.country_name, hf_table.hf_score, out_of_school.out_of_school
 FROM hf_table
 JOIN out_of_school
-ON out_of_school.country_code=hf_table.country_code;
+ON out_of_school.country_code=hf_table.country_code
+ORDER BY out_of_school.out_of_school DESC;
